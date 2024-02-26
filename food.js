fa6b87e963aec808d285423bb2e78dffef5d48ee
Mark Garcia <155798583+jshmrk@users.noreply.github.com>
2024-02-26 15:11:20 +0800

    Update food.js

--- food.js
+++ food.js
@@ -85,6 +85,16 @@ $(document).ready(function ($) {
 
 });
 
+$('a[href^="#"]').on('click', function (event) {
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 800);
+});
+
+$(window).on('load', function () {
+    $('.loading-screen').fadeOut('slow');
+});
 
 jQuery(window).on('load', function () {
     $('body').removeClass('body-fixed');
