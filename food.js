f365866057e4f4bf6f37b5dd4a21db81ac9a1ec2
EliiiBaltazar <155798304+EliiiBaltazar@users.noreply.github.com>
2024-02-29 12:18:46 +0800

    Update food.js

--- food.js
+++ food.js
@@ -89,9 +89,6 @@ $(document).ready(function ($) {
     var scene = $(".js-parallax-scene").get(0);
     var parallaxInstance = new Parallax(scene);
 
-
-});
-
 $('a[href^="#"]').on('click', function (event) {
     event.preventDefault();
     $('html, body').animate({
